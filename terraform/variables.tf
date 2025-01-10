9ffdc68f2201b912ac46fad2349ed1da9e1a8d80
nikolaivanovj <110764167+nikolaivanovj@users.noreply.github.com>
2025-01-10 20:03:30 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,18 @@ variable "kubernetes_cluster_name" {
   description = "The name of the Kubernetes cluster"
   default     = "gethdevnetaks"
 }
+
+variable "disk_size_gb" {
+  description = "The size of the Azure managed disk in gigabytes"
+  default     = 10
+}
+
+variable "node_count" {
+  description = "The number of nodes in the Kubernetes cluster"
+  default     = 1
+}
+
+variable "vm_size" {
+  description = "The size of the virtual machines in the Kubernetes cluster"
+  default     = "Standard_DS2_v2"
+}

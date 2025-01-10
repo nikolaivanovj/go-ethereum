c0a2f24abb21df37bb54b1f36f4467842f8d14dc
nikolaivanovj <110764167+nikolaivanovj@users.noreply.github.com>
2025-01-10 20:02:15 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,47 @@ resource "azurerm_kubernetes_cluster" "gethdevnetaks" {
   }
 }
 
+resource "azurerm_managed_disk" "geth_disk" {
+  name                 = "geth-disk"
+  location             = azurerm_resource_group.gethdevnetrg.location
+  resource_group_name  = azurerm_resource_group.gethdevnetrg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 10
+}
+
+resource "kubernetes_persistent_volume" "geth_pv" {
+  metadata {
+    name = "geth-data-pv"
+  }
+  spec {
+    capacity {
+      storage = "10Gi"
+    }
+    access_modes = ["ReadWriteOnce"]
+
+    azure_disk {
+      disk_name    = azurerm_managed_disk.geth_disk.name
+      disk_uri     = azurerm_managed_disk.geth_disk.id
+      kind         = "Managed"
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "geth_pvc" {
+  metadata {
+    name = "geth-data-pvc"
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests {
+        storage = "10Gi"
+      }
+    }
+  }
+}
+
 output "kube_config" {
   value     = azurerm_kubernetes_cluster.gethdevnetaks.kube_config_raw
   sensitive = true

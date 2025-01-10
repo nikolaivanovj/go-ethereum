9ada5de684ed38b6335b5959728b750599e28887
nikolaivanovj <110764167+nikolaivanovj@users.noreply.github.com>
2025-01-10 20:54:07 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -27,3 +27,19 @@ variable "vm_size" {
   description = "The size of the virtual machines in the Kubernetes cluster"
   default     = "Standard_DS2_v2"
 }
+
+variable "subscription_id" {
+  description = "The subscription ID for the Azure account"
+}
+
+variable "client_id" {
+  description = "The client ID for the Service Principal"
+}
+
+variable "client_secret" {
+  description = "The client secret for the Service Principal"
+}
+
+variable "tenant_id" {
+  description = "The tenant ID for the Azure account"
+}
